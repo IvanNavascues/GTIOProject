33dc1400673b5dfe45124c8c179f4997228ec146
upna-albertomc <martinez.178711@e.unavarra.es>
2026-02-22 17:07:29 +0100

    Frontend con estilos

---
+++ frontend/actualizarNominados.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+
+    var leyenda = {
+        "01": "Carlos Fustel",
+        "02": "Claudia Arenas",
+        "03": "Cristina Lora",
+        "04": "Elena Matateyou",
+        "05": "Guille Toledano",
+        "06": "Guillo Rist",
+        "07": "Ivan Rojo",
+        "08": "JaviCrespo",
+        "09": "JuditGaruz",
+        "10": "Laura Munyoz",
+        "11": "Lucia Casani",
+        "12": "Maria Cruz",
+        "13": "Martin Yanyez",
+        "14": "Olivia Bay",
+        "15": "Salma De Diego"
+    }
+
+    /*
+    $.ajax({
+        type: "GET",
+        url: "/votes",
+        success: function (data) {
+            // depende de la estructura de data
+            update(data)
+        }
+    });
+    */
+
+    var data = {
+        "nominado_uno": "07",
+        "nominado_dos": "04"
+    }
+    update(data)
+
+    function update(data) {
+        for (let i = 0; i < Object.keys(data).length; i++) {
+
+            var nominado = Object.keys(data)[i]
+            var id = data[nominado]
+            var img_source = "imagenes/" + id + ".webp"
+            var nombre = leyenda[id]
+
+            var col = document.createElement("div")
+            col.className = "col"
+            var row1 = document.createElement("div")
+            row1.className = "row"
+            var row2 = document.createElement("div")
+            row2.className = "row"
+            var row3 = document.createElement("div")
+            row3.className = "row"
+
+            var img = document.createElement("img")
+            img.src = img_source
+
+            var label = document.createElement("label")
+            label.id = "nominado_" + id
+            label.textContent = nombre
+
+            var input = document.createElement("input")
+            input.type = "radio"
+            input.name = "voto"
+            input.value = id
+
+            row1.appendChild(img)
+            row2.appendChild(label)
+            row3.appendChild(input)
+
+            col.appendChild(row1)
+            col.appendChild(row2)
+            col.appendChild(row3)
+
+            document.getElementById("nominados").appendChild(col)
+        }
+    }
+
+
+
+})
--- frontend/votacion.js
+++ frontend/votacion.js
@@ -1,38 +1,19 @@
 $(document).ready(function () {
 
     $("#submit").click(function () {
-        //var email = $("#campoCorreo").val()
-        //var pass = $("#campoContrasenya").val()
 
         var voto = ""
         voto = $('input[name="voto"]:checked').val();
-
-        //console.log("email: " + email)
-        //console.log("voto: " + voto)
 
         if (voto == null) {
             $("#avisoFormulario").text("Seleccionar una opcion.")
         }
         else {
             enviarVoto(voto)
-            /*
-            if ($validarCorreo(email) && $validarPass(pass)) {
-                $enviarVoto(email, pass, voto)
-            }
-            else {
-                $("#avisoFormulario").text("email o contrasenya invalida.")
-            }
-            */
         }
 
     })
 
-    $validarPass = function (pass) {
-        if (pass.length < 3 || pass.length > 15) {
-            return false
-        }
-        return true
-    }
 
     $enviarVoto = function (voto) {
         var GETurl = "/vote/" + voto
@@ -47,18 +28,4 @@ $(document).ready(function () {
 
     }
 
-    /*
-        $validarCorreo = function (email) {
-            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-            return regex.test(email);
-    
-        }
-    
-        $enviarVoto = function (email, pass, voto) {
-            $("#avisoFormulario").text("")
-            console.log("comprobar email")
-            console.log("votado por " + voto)
-    
-        }
-    */
 });
